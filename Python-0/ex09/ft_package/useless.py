4862434676c93f488f32a64a2d30e6c3de6d1fb3
alfux <alexis.t.fuchs@gmail.com>
2024-02-24 04:21:24 +0100

    + __doc__ on ex09 	+ Python-1

--- Python-0/ex09/ft_package/useless.py
+++ Python-0/ex09/ft_package/useless.py
@@ -1,10 +1,13 @@
 def count_in_list(lst: list, wht: any) -> int:
+    """Counts number of items in a list"""
     return (lst.count(wht))
 
 
 def print_legend() -> None:
+    """Yes, like in the movie"""
     print("I am legend.")
 
 
 def double_sandwich(x: any) -> any:
+    """Double it, double it, double it again"""
     return (x + x)
