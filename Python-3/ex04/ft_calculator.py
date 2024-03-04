2afdcf858b286b6ca08c9a17b2015dc22f7502cf
alfux <alexis.t.fuchs@gmail.com>
2024-03-04 05:25:30 +0100

    + ex04

---
+++ Python-3/ex04/ft_calculator.py
@@ -0,0 +1,23 @@
+class calculator:
+    """Process operations on two vectors."""
+    def __init(self):
+        """Instanciate an empty calculator object.."""
+        pass
+
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """Prints dot product of two vectors."""
+        new = [x * y for (x, y) in zip(V1, V2)]
+        print(f"Dot product is: {sum(new)}")
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """Prints sum of two vectors."""
+        new = [float(x + y) for (x, y) in zip(V1, V2)]
+        print(f"Add Vector is: {new}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """Prints difference of two vectors."""
+        new = [float(x - y) for (x, y) in zip(V1, V2)]
+        print(f"Sous Vector is: {new}")
